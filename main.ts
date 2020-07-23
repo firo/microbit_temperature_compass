e7e5ad510ece9d50b7b27687c648f9e737c5948d
Firo <mail@firo.it>
2020-07-23 09:49:24 +0200

    fix main menu

--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
-function goNext () {
-    if (listCur == list.length) {
-        listCur = 0
-        basic.showString("" + (list[listCur]))
-    } else {
-        basic.showString("" + (list[listCur]))
-    }
-    listCur = listCur + 1
-}
 function doStart () {
     list = [1, 2, 3]
     listCur = 0
-    basic.showIcon(IconNames.Happy)
+    while (menu_home == true) {
+        basic.showIcon(IconNames.Happy)
+    }
+}
+function showNext () {
+    menu_home = false
+    if (listCur == list.length) {
+        listCur = 0
+    }
+    basic.showString("" + (list[listCur]))
+    listCur = listCur + 1
 }
 input.onButtonPressed(Button.A, function () {
+    menu_home = false
     if (listCur == 1) {
         doTemperature()
     } else if (listCur == 2) {
@@ -22,7 +24,9 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 function doTemperature () {
-    while (true) {
+    menu_home = false
+    while (listCur == 1) {
+        basic.clearScreen()
         basic.showNumber(input.temperature())
         basic.pause(1000)
         basic.clearScreen()
@@ -31,12 +35,12 @@ function doTemperature () {
         20,
         40
         )
-        basic.pause(2000)
-        basic.clearScreen()
+        basic.pause(1000)
     }
 }
 function doCompass () {
-    while (true) {
+    menu_home = false
+    while (listCur == 2) {
         if (input.compassHeading() > 315 || input.compassHeading() < 45) {
             basic.showString("N")
         } else if (input.compassHeading() < 135) {
@@ -49,11 +53,15 @@ function doCompass () {
     }
 }
 input.onButtonPressed(Button.AB, function () {
+    menu_home = true
     doStart()
 })
 input.onButtonPressed(Button.B, function () {
-    goNext()
+    menu_home = false
+    showNext()
 })
-let list: number[] = []
 let listCur = 0
+let list: number[] = []
+let menu_home = false
+menu_home = true
 doStart()
